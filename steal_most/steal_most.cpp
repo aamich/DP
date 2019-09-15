70a8f0929cc7905a323c54438c6edd4bd731a82c
aamich <aamich@yahoo.com>
2019-09-14 23:35:20 -0700

    Steal most but no immediate neighbor ;)

---
+++ steal_most/steal_most.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> IntVec;
+
+int stealMost(const IntVec& arr, int n) {
+    IntVec Opt(arr.size(), 0);
+    Opt[0] = arr[0];
+    Opt[1] = arr[1];
+    if(n < 2)
+        return Opt[n];
+    for(int i = 2; i < n; ++i)
+        Opt[i] = max(arr[i] + Opt[i-2], Opt[i-1]);
+    return Opt[n-1];
+}
+
+int main()
+{
+    IntVec arr{1, 2, 4, 1, 7, 8, 3};
+    cout << stealMost(arr, 4) << endl;
+    cout << stealMost(arr, 5) << endl;
+    cout << stealMost(arr, 6) << endl;
+    cout << stealMost(arr, 7) << endl;
+}
